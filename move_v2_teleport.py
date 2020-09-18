f28796c72a54c2ba54239cca61ec92b7109382bf
fzramos <64625115+fzramos@users.noreply.github.com>
2020-09-17 22:24:11 -0500

    Adding teleport
    Adds the snake ability to move from end of the board to other end

--- move_v2_teleport.py
+++ move_v2_teleport.py
@@ -2,22 +2,31 @@ import numpy as np
 import board1
 
 def user_move():
-    move = input("turn snake left(l), right(r), or no change (n): ")
+    move = input("turn snake up(u), down(d), left(l), right(r), or no change (n): ")
     return move
 
 def update_direct(direct_change, direct):
     changes = {
-        "westl": "south",
-        "westr": "north",
+        "northu": "north",
+        "northd": "north",
         "northl": "west",
         "northr": "east",
-        "eastl": "north",
-        "eastr": "south",
-        "southl": "east",
-        "southr": "west",
+        "eastu": "north", 
+        "eastd": "south", 
+        "eastl": "east",       
+        "eastr": "east",
+        "southu": "south",
+        "southd": "south",
+        "southl": "west",
+        "southr": "east",
+        "westu": "north",
+        "westd": "south",
+        "westl": "west",
+        "westr": "west"
     }
     new_direct = direct
-    if direct_change != "n":
+    #find a better way to say( if anythig  typed in besides u,d,l,r change nothing)
+    if (direct_change != "") and (direct_change in "udlr"):
         new_direct = changes[direct + direct_change]
 
     return new_direct
@@ -46,6 +55,15 @@ def change_pos(dimen, direct, snake_pos):
         snake_head[0,] += 1
     else:
         snake_head[1,] -= 1
+    #find a way to optimize this (probably for loop)
+    if snake_head[0,]==-1:
+        snake_head[0,]=dimen[0]-1
+    elif snake_head[0,]==dimen[0]:
+        snake_head[0,]=0
+    if snake_head[1,]==-1:
+        snake_head[1,]=dimen[1]-1
+    elif snake_head[1,]==dimen[1]:
+        snake_head[1,]=0
 
     new_snake = np.insert(new_snake,0 , snake_head, axis = 0)
     return(new_snake)
@@ -65,13 +83,10 @@ def print_board_stuff(move_data):
 
 def game(dimen=(5,5)):
     #defining initial values for variables
-    #board_mat = np.zeros(size)
     #first value in list is snake head, last is snake end
     snake_pos = [0,2,0,1,0,0]
     snake_pos = np.array(snake_pos).reshape((3,2))
-    #board_mat[0, 0] = 1
-    #board_mat[0, 1] = 1
-    #board_mat[0, 2] = 2
+
     board_mat = update_mat_snake(dimen, snake_pos)
 
     direct = "east"
@@ -81,7 +96,7 @@ def game(dimen=(5,5)):
     print_board_stuff(move_data)   
 
     i=0
-    while i<10:
+    while i<100:
         direct_change = user_move()
         move_data = movement(dimen, move_data[0], direct_change, move_data[1], move_data[2])
         print_board_stuff(move_data)
